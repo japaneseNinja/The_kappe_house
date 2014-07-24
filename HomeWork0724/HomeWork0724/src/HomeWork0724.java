c539e3cd67689d571faabe89d6a76c7b780218b4
Tsubaki <Tsubaki@Tsubaki-PC>
2014-07-25 07:13:18 +0900

    完成

---
+++ HomeWork0724/HomeWork0724/src/HomeWork0724.java
@@ -0,0 +1,29 @@
+
+public class HomeWork0724 {
+public static void main(String[] args) {
+	String name = "";
+	KappeStopper stop = new KappeStopper();
+	stop.check(name);
+
+}
+}
+
+class KappeStopper{
+	void check(String name){
+		if("鈴村".equals(name)){
+			try {
+				throw  new SuzumuraException();
+			} catch (SuzumuraException e) {
+				e.printStackTrace();
+			}
+				}
+		else{
+			System.out.println("通行許可");
+			}
+	}
+}
+class SuzumuraException extends Exception{
+	SuzumuraException(){
+	System.out.println("鈴村禁止！");
+}
+}
